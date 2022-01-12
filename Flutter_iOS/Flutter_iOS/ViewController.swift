475b7b21026f3bc4b5d1c7946c42115dc3956acb
xzchengz <974897435@qq.com>
2022-01-13 07:45:01 +0800

    原生APP添加flutter

--- Flutter_iOS/Flutter_iOS/ViewController.swift
+++ Flutter_iOS/Flutter_iOS/ViewController.swift
@@ -6,14 +6,31 @@
 //
 
 import UIKit
+import Flutter
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .red
     }
-
+    
+    @objc func showFlutter() {
+        let flutterEngine = (UIApplication.shared.delegate as! AppDelegate).flutterEngine
+        let flutterViewController = FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
+        present(flutterViewController, animated: true, completion: nil)
+    }
+    
+    @objc func showFlutter1() { // 内嵌引擎，每次启动都很慢。不建议使用，除非业务几百年不用一次的那种。
+        let flutterViewController = FlutterViewController(project: nil, nibName: nil, bundle: nil)
+        present(flutterViewController, animated: true, completion: nil)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.showFlutter()
+    }
 
 }
 
