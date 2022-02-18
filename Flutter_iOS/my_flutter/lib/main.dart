0c607ede565b510c1181b58b3598a9f463a69258
chengz <chengzhang68@creditease.cn>
2022-02-18 22:05:41 +0800

    2.18

--- Flutter_iOS/my_flutter/lib/main.dart
+++ Flutter_iOS/my_flutter/lib/main.dart
@@ -5,8 +5,70 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
+class MyAppBar extends StatelessWidget {
+  const MyAppBar({required this.title, Key? key}) : super(key: key);
+
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Container(
+      height: 56.0, // 单位是逻辑上的像素（并非真实的像素，类似于浏览器中的像素）
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: new BoxDecoration(color: Colors.blue[500]),
+      // Row 是水平方向的线性布局（linear layout）
+      child: new Row(
+        //列表项的类型是 <Widget>
+        children: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+            onPressed: null, // null 会禁用 button
+          ),
+          // Expanded expands its child to fill the available space.
+          new Expanded(
+            child: title,
+          ),
+          new IconButton(
+            icon: new Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Myscaffold extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return new Material(
+      child: new Column(
+        children: <Widget>[
+          new MyAppBar(
+            title: new Text(
+              'Example title',
+              style: Theme.of(context).primaryTextTheme.headline6,
+            ),
+          ),
+          new Expanded(
+              child: new Center(
+            child: new Text('Hello, world!'),
+          )),
+        ],
+      ),
+    );
+  }
+}
+
 void main() {
-  runApp(const MyApp());
+  runApp(new MaterialApp(
+    title: 'My app',
+    home: new Myscaffold(),
+  ));
+
+  // runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
